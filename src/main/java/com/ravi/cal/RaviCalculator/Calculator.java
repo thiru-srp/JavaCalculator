df047cfc8f67791f65575449f9b8e6da20bacace
THIRU <thirupati.marisetti@gmail.com>
2022-04-15 13:32:52 +0530

    add mul method

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -29,9 +29,9 @@ public class Calculator {
 	}
 	
 
-        public long divFucn(long first, long second){
+        public long mulFucn(long first, long second){
 
-                return first/second;
+                return first*second;
         }
 	
 	public static void main(String[] args) {
