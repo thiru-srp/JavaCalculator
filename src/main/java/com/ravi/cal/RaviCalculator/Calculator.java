425d36d191e4f8a12af320b86266ed9d23d89d86
THIRU <thirupati.marisetti@gmail.com>
2022-04-15 13:27:11 +0530

    div added & mul removed

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -28,15 +28,11 @@ public class Calculator {
 		return second-first;
 	}
 	
-	public long mulFucn(long first, long second){
-		
-		return first*second;
-	}
 
         public long divFucn(long first, long second){
 
                 return first/second;
-        }	
+        }
 	
 	public static void main(String[] args) {
 
